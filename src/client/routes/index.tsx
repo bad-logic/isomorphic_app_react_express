712cda1108f71a0951bdb0f1e014d84753799c95
roshan <mailme.roshanbhattarai@gmail.com>
2021-11-24 20:20:05 +0545

    webpack configurations for client and server

--- src/client/routes/index.tsx
+++ src/client/routes/index.tsx
@@ -1,12 +1,15 @@
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../pages/home";
+import NotFound from "../pages/404";
 
-const Routes: React.FC = () => {
+const AppRoutes: React.FC = () => {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route component={NotFound} />
     </Switch>
   );
 };
 
-export default Routes;
+export default AppRoutes;
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,11 +3,11 @@ import { serverSideRendering } from "./middlewares/ssr";
 
 const app = express();
 
-app.get("/*", serverSideRendering);
-
 app.get("/success", (req, res) => {
   res.send("success");
 });
+
+app.get("/*", serverSideRendering);
 
 app.listen(3000, () => {
   console.log("app started at port 3000");
