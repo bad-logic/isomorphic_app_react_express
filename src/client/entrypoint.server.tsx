6111aeebbb854e923e0d579b3eee8a565b5266eb
roshan <mailme.roshanbhattarai@gmail.com>
2021-11-25 08:37:44 +0545

    added prettier

--- src/client/entrypoint.server.tsx
+++ src/client/entrypoint.server.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { StaticRouter } from "react-router-dom";
-import AppRoutes from "./routes";
+import React from 'react';
+import { StaticRouter } from 'react-router-dom';
+import AppRoutes from './routes';
 
 interface ServerProps {
   location: string;
--- src/client/routes/index.tsx
+++ src/client/routes/index.tsx
@@ -1,28 +1,28 @@
-import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
-import Home from "../pages/home";
-import NotFound from "../pages/404";
-import ContactUs from "../pages/contact";
-import AboutUs from "../pages/about";
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import Home from '../pages/home';
+import NotFound from '../pages/404';
+import ContactUs from '../pages/contact';
+import AboutUs from '../pages/about';
 
 const AppRoutes: React.FC = () => {
   return (
     <>
       <ul>
         <li>
-          <Link to={"/"}>Home</Link>
+          <Link to={'/'}>Home</Link>
         </li>
         <li>
-          <Link to={"/contact-us"}>contact</Link>
+          <Link to={'/contact-us'}>contact</Link>
         </li>
         <li>
-          <Link to={"/about-us"}>About</Link>
+          <Link to={'/about-us'}>About</Link>
         </li>
       </ul>
       <Switch>
-        <Route exact path={"/"} component={Home} />
-        <Route path={"/about-us"} component={AboutUs} />
-        <Route path={"/contact-us"} component={ContactUs} />
+        <Route exact path={'/'} component={Home} />
+        <Route path={'/about-us'} component={AboutUs} />
+        <Route path={'/contact-us'} component={ContactUs} />
         <Route component={NotFound} />
       </Switch>
     </>
