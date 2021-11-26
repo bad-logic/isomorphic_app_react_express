77df38178f584c167d9837f84ae0b87ef9918ae5
roshan <mailme.roshanbhattarai@gmail.com>
2021-11-26 08:44:15 +0545

    adding title tags in browser

--- src/client/pages/about/index.tsx
+++ src/client/pages/about/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { useReduxState } from '../../hooks/useRedux';
+import { TEMPLATE_TITLES } from '../../../server/utils/seo';
+
 const App = () => {
   // const { isomorphic } = useReduxState();
+
+  React.useEffect(() => {
+    document.getElementsByTagName('title')[0].innerHTML =
+      TEMPLATE_TITLES['about-us'];
+  }, []);
 
   return <div>ABOUT OUR TEAM</div>;
 };
